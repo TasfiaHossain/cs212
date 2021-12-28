589f19d7b5b1c0254ca3f57adfce2629a70cd892
Tasfia Hossain <66657441+TasfiaHossain@users.noreply.github.com>
2021-12-28 16:09:25 -0500

    Add files via upload

---
+++ Project1/Project1.java
@@ -0,0 +1,13 @@
+//@Tasfia Hossain
+//@Project 1
+//@3/12/2021
+//Lab Section 11E
+
+//@param: in the main method declare the GUI 
+
+public class Project1 {
+	public static void main(String[] args) {
+		NumbersGUI GUI = new NumbersGUI();
+		NumbersGUI.printarray(GUI); //will display in the GUI and print the sequence in each text box
+	}
+}
